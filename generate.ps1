58a91800b960782c99b6970c6d3bf08b75a4a2d4
Dainius Dauksevicius <dainius.dauksevicius@gmail.com>
2021-01-12 12:24:36 +0200

    improve generate script

--- generate.ps1
+++ generate.ps1
@@ -1,5 +1,11 @@
+$startTime = (Get-Date);
 cd backend;
 npm start;
 cp results.json ../frontend/content;
 cd ../frontend;
 npm run build;
+cd ..;
+$endTime = (Get-Date);
+Write-Host
+Write-Host 'Success' -ForegroundColor Green
+Write-Host ('Total time: {0:mm} min {0:ss} sec' -f ($endTime - $startTime) )
